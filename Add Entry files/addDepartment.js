83a7b45370caf55b0ac36fbab801dbbf45edbc5e
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-27 23:07:21 +0930

    Updated Add Department with await.queryDB

--- Add Entry files/addDepartment.js
+++ Add Entry files/addDepartment.js
@@ -1,18 +1,21 @@
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
+const db = require('./db');
+const queryDB = require("./utils");
 
-    const addDepartment = async () => {
-        const departmentInput = await inquirer.prompt ([
-            {
-                name: 'department',
-                type: 'input',
-                message: 'Please enter the name of the department you would like to add:'
-            }
-        ]);
-        const {department} = departmentInput;
-        const departmentName = new departmentName (department);
-        employeeInformation.push(departmentInput);
-        console.log(departmentName); 
-        callPrompts();
-    }
+// Function to post new department
+const addDepartment = async () => {
+  // Prompt for title of department being stored
+    const answer = await inquirer.prompt ([
+        {
+            name: 'department',
+            type: 'input',
+            message: 'Please enter the name of the department you would like to add:'
+        }
+    ])
+    // Insert new department into DB
+    await queryDB('INSERT INTO department SET department_name = ?',[ answer.department ])
+    console.log('You have successfully created a new department!')
+    addEntry();
+};
 
 module.exports = addDepartment;
