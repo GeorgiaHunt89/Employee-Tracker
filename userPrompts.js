f7653829067e1880762fafa85152efd402138b76
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-27 14:23:36 +0930

    Added user Prompts page for choices

---
+++ userPrompts.js
@@ -0,0 +1,44 @@
+const userPrompts = [
+{
+    type: 'list',
+    name: 'choice',
+    message: 'What would you like to choose?',
+    choices: [
+        {
+            name: 'View all departments'
+            value: 'View_Departments'
+        },
+        {
+            name: 'View all roles'
+            value: 'View_Roles'
+        },
+        {
+            name: 'View all employees'
+            value: 'View_Employees'
+        },
+        {
+            name: 'Add a department'
+            value: 'Add_Department'
+        },
+        {
+            name: 'Add a role'
+            value: 'Add_Role'
+        },
+        {
+            name: 'Add an employee'
+            value: 'Add_Employee'
+        },
+        {
+            name: 'Update employees'
+            value: 'Update_Employees'
+        },
+        {
+            name: 'Quit'
+            value: 'Quit'
+        },
+    ]
+}
+
+];
+
+module.exports = userPrompts;
