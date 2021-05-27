6c86002abaa12f1ebb1b36a56cc4df248f3fb576
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-27 23:06:50 +0930

    Moved Add Entry function into own file

---
+++ addEntry.js
@@ -0,0 +1,25 @@
+
+// Creates database records
+// Add Entry function 
+const addEntry = async () => {
+    const addEntryInput = await inquirer.prompt({
+                    type: 'list',
+                    name: 'selectEntryType',
+                    message: 'What type of entry to you wish to add?',
+                    choices: ['Add Department', 'Add Role', 'Add Employee'],
+            });
+// Calls function chosen by user 
+switch (addEntryInput.entryInputSelection) {
+    case 'addDepartment':
+            addDepartment();
+            break;
+    case 'addRole':
+            addRole(); 
+            break;
+    case 'addEmployee':
+            addEmployee(); 
+            break;      
+}
+console.log('You have chosen ', addEntryInput.entryInputSelection);
+};
+
