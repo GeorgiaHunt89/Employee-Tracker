b05012bbc7a84e214dc9f74607b57bedec71136f
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-27 14:04:54 +0930

    Created connection to MYSQL

---
+++ connection.js
@@ -0,0 +1,16 @@
+// Create connection to MYSQL
+
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employee_trackerDB',
+});
+
+connection.connect((error) => {
+    if (error) throw error;
+});
+
+module.exports = connection;
