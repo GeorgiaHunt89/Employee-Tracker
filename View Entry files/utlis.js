976238a9ff164c9df6d77383f7efac2a19b10bd5
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-28 13:22:27 +0930

    Utlis folder added

---
+++ View Entry files/utlis.js
@@ -0,0 +1,16 @@
+const connection = require("../connection");
+
+async function queryDB(query, data = []) {
+    return new Promise((resolve, reject) => { 
+        connection.query(
+            query,
+            data,
+            (error, results) => {
+                if (error) { reject(error); }
+                resolve(results);
+            }
+        );
+    })
+}
+
+module.exports = queryDB;
