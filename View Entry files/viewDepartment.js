f90feed82d6514d9c517651e49d68a30c0a73735
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-28 13:22:17 +0930

    view Department updated

---
+++ View Entry files/viewDepartment.js
@@ -0,0 +1,18 @@
+const queryDB = require('./utlis');
+const inquirer = require("inquirer");
+
+// Function to view all departments
+const viewAllDepartments = async () => {
+    // Prompt for department search list
+      const answer = await inquirer.prompt ([
+          {
+              name: 'department',
+              type: 'input',
+              message: 'Please enter the name of the department you would like to view:'
+          }
+      ])
+      // View department listing stored into DB
+      await queryDB('SELECT * FROM department',[ answer.department ])
+      console.table(`Department: ${department}`)
+  };
+  viewAllDepartments();
