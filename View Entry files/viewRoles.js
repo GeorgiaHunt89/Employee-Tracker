2fb809d3077afadcbfba9af7d164a1758a03cd1a
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-28 13:21:58 +0930

    viewAllRoles function added

---
+++ View Entry files/viewRoles.js
@@ -0,0 +1,18 @@
+const queryDB = require('./utlis');
+const inquirer = require("inquirer");
+
+// Function to view all roles
+const viewAllRoles = async () => {
+    // Prompt for role search list
+      const answer = await inquirer.prompt ([
+          {
+              name: 'role',
+              type: 'input',
+              message: 'Please enter the name of the role you would like to view:'
+          }
+      ])
+      // View role listing stored into DB
+      await queryDB('SELECT * FROM role',[ answer.role ])
+      console.table(`Role: ${role}`)
+  };
+  viewAllRoles();
