cde1ce979170eccfb37d86a780ba19254bcf1dcf
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-28 13:34:15 +0930

    View all employees by role function added

--- Add Entry files/addEmployee.js
+++ Add Entry files/addEmployee.js
@@ -1,7 +1,5 @@
-const inquirer = require("inquirer");
 const queryDB = require("./utils");
-
-    
+ 
 
 // Function to add new employee
 const addEmployee = async () => {
---
+++ View Entry files/viewEmployeesByRole.js
@@ -0,0 +1,18 @@
+const queryDB = require('./utlis');
+const inquirer = require("inquirer");
+
+// Function to view all employees by role
+const viewAllEmployeesByRole = async () => {
+    // Prompt for role search list
+      const answer = await inquirer.prompt ([
+          {
+              name: 'role_id',
+              type: 'input',
+              message: 'Please enter the role id you would like to view employees by:'
+          }
+      ])
+      // View role listing stored into DB
+      await queryDB('SELECT role_id FROM employee',[ answer.role_id ])
+      console.table(`Role_id: ${role_id}`)
+  };
+  viewAllEmployeesByRole();
