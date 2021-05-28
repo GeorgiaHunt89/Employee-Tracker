7a7e84c5bc1749848a8b2b16179c89707439d56a
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-28 13:31:40 +0930

    Function to view roles by department added

---
+++ View Entry files/viewRolesByDepartment.js
@@ -0,0 +1,18 @@
+const queryDB = require('./utlis');
+const inquirer = require("inquirer");
+
+// Function to view all roles by department
+const viewAllRolesByDepartment = async () => {
+    // Prompt for role search list
+      const answer = await inquirer.prompt ([
+          {
+              name: 'department_id',
+              type: 'input',
+              message: 'Please enter the department id you would like to view roles:'
+          }
+      ])
+      // View role listing stored into DB
+      await queryDB('SELECT department_id FROM role',[ answer.department_id ])
+      console.table(`Department_id: ${department_id}`)
+  };
+  viewAllRolesByDepartment();
