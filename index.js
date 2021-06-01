0dda106084792b581f059384ead4b9f23b730c55
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-01 13:30:30 +0930

    Updated index

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 const inquirer = require('inquirer');
-// const db = require('./db');
 const connection = require('./connection');
 const addEntry = require('./Add Entry files/addEntry');
+const viewEntry = require('./View Entry files/viewEntry');
+const updateEntry = require('./Update Entry files/updateEntry');
 const userPrompts = require('./userPrompts');
 
 
 // Function to load prompts in NPM
 const callPrompts = async () => {
         console.log('Welcome to the Employee Tracker')
-        const userInput = await inquirer.prompt(userPrompts);
+        const userPrompts = await inquirer.prompt();
 
         // Calls function chosen by user 
-        switch (userInput.inputSelection) {
+        switch (userPrompts.inputSelection) {
         case 'addEntry':
-                return addEntry();
+                addEntry();
+                break;
         case 'viewEntry':
-                return viewEntry(); 
+                viewEntry(); 
+                break;
         case 'updateEntry':
-                return updateEntry(); 
+                updateEntry();
+                break; 
         default:
                 return connection.end();         
         }
+
 };
 
 callPrompts();
-
 
