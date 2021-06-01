623940f11a6f2dfb2c0a75c9d8c63f9c2180f42a
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-01 15:01:30 +0930

    Delete department funciton added

---
+++ Delete files/deleteDepartment.js
@@ -0,0 +1,19 @@
+const queryDB = require('./utils');
+const inquirer = require("inquirer");
+
+
+// Function to view all departments
+const deleteDepartment = async () => {
+    // Prompt for department search list
+      const answer = await inquirer.prompt ([
+          {
+              name: 'department',
+              type: 'input',
+              message: 'Please enter the name of the department you would like to delete:'
+          }
+      ])
+      // Delete department listing stored into DB
+      await queryDB('DELETE FROM department WHERE department = ?',[ answer.department ])
+      console.table(`Department: ${department}`)
+  };
+  deleteDepartment();
