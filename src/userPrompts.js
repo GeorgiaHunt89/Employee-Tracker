3456dff91a2994bad15fc53463e23fdd81ab1f4d
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-01 23:42:50 +0930

    Added async and await

--- src/userPrompts.js
+++ src/userPrompts.js
@@ -1,28 +1,35 @@
-const userPrompts = [
-{
-    type: 'list',
-    name: 'inputChoices',
-    message: 'What action would you like to complete today?',
-    choices: [
-        {
-            name: 'Add an Entry',
-            value: 'addEntry',
-        },
-        {
-            name: 'View an Entry',
-            value: 'viewEntry',
-        },
-        {
-            name: 'Update an Entry',
-            value: 'updateEntry',
-        },
-        {
-            name: 'Delete an Entry',
-            value: 'deleteEntry',
-        },
-    ]
-}
 
-];
+const { prompt } = require('inquirer');
+
+const userPrompts = async () => {
+
+    const answer = await prompt(
+        {
+            type: 'list',
+            name: 'inputChoices',
+            message: 'What action would you like to complete today?',
+            choices: [
+                {
+                    name: 'Add an Entry',
+                    value: 'addEntry',
+                },
+                {
+                    name: 'View an Entry',
+                    value: 'viewEntry',
+                },
+                {
+                    name: 'Update an Entry',
+                    value: 'updateEntry',
+                },
+                {
+                    name: 'Delete an Entry',
+                    value: 'deleteEntry',
+                },
+            ]
+        });
+
+    return answer.inputChoices;
+
+};
 
 module.exports = userPrompts;
